7b91a65d8d3e468821cfb9a3508a5f2a88a9982b
shahjalal <shahjalal@pop-os.localdomain>
2021-05-23 13:03:35 +0600

    css fixd

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ routes(app);
 database(app);
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === 'production' || 'development') {
+if (process.env.NODE_ENV === 'production') {
 	// Set static folder
 	app.use(express.static('client/build'));
 
